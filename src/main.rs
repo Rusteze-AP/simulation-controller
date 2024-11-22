a399d038f7de0b4c9ec3309824ddd79c30b9108f
achf01 <annachiarafortuna01@gmail.com>
2024-11-22 22:22:06 +0100

    initial integration with network initializer

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 slint::include_modules!();
 use rfd::FileDialog;
+use std::ffi::OsString;
 use network_initializer::{NetworkInitializer};
 // use slint::Model;
 
@@ -8,26 +9,31 @@ fn main() -> Result<(), slint::PlatformError> {
 
     let main_window = MainWindow::new()?;
 
-    main_window.on_clicked(move || {
-        println!("CIAO");
+    main_window.on_open_right_window(move || {
+        println!("[] OPEN RIGHT WINDOW");
+        // TODO -> spawn the rigth window
+
+        // let right_menu : RightMenu = RightMenu::new();
+        // right_menu.run(); // Show as a separate window
+
     });
 
     main_window.on_select_file(move || {
         println!("[] FILE SELECTION");
         let file = FileDialog::new().pick_file();
         if let Some(mut path)= file {
-            println!("File selected: {:?}", path.as_mut_os_str());
-            // INTEGRARE CODICE NEWTWORK INITIALIZER
-
-            // // TO IMPLEMENT the spawn of the drones
-            // let mut network = NetworkInitializer::new(Some(path));
-            // let config = NetworkInitializer::new(Some(path));
-            // assert!(config.is_ok(), "{}", config.err().unwrap());
-            // let config = config.unwrap();
-            // // println!("{config:#?}");
-            // config.run_simulation();
-
-
+            let path_string = <OsString as Clone>::clone(&path.as_mut_os_string()).into_string();
+            if let Ok(path_string) = path_string {
+                println!("Selected file: {}", path_string);
+                let mut network = NetworkInitializer::new(Some(path_string.as_str()));
+                let config = NetworkInitializer::new(Some(&path_string));
+                // assert!(config.is_ok(), "{}", config.err().unwrap());
+                // let config = config.unwrap();
+                // println!("{config:#?}");
+                // config.run_simulation();
+            }else{
+                println!("Error converting path to string");
+            }
         }else{
             println!("No file selected\nTry again");
         }
